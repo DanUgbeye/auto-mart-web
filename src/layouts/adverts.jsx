9c7af49100a48e1c65cdc0d3898d68c5865a4500
Daniel Ugbeye <ugbeyellionz@gmail.com>
2022-11-04 21:34:35 +0100

    fix: fixed warning with unused vars

--- src/layouts/adverts.jsx
+++ src/layouts/adverts.jsx
@@ -5,16 +5,6 @@ import Heading from "../components/heading";
 const Adverts = () => {
   return (
     <section className="relative min-h-[100vh] sm:min-h-[100vh] ">
-      {/* <Heading
-        heading={"Adverts"}
-        supportText={"find created adverts or create a new one"}
-        extraStyle={
-          " px-8 py-2 text-navy-blue sticky top-[3rem] sm:top-0 left-0 right-0 z-[1000] shadow-black/10 shadow-md relative "
-        }
-        headingStyle={" text-4xl mb-2  "}
-        supportTextStyle={" text-lg "}
-      /> */}
-
       <Outlet />
     </section>
   );
